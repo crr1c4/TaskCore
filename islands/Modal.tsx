814bd51c49781607ae89a2ff0b5272bd5a4f554a
Christian Venegas <venegaschristian014@gmail.com>
2025-03-24 08:44:18 -0600

    Formateo del archivo

--- islands/Modal.tsx
+++ islands/Modal.tsx
@@ -15,10 +15,12 @@ export function ModalError({ mensaje }: Props) {
             <h2 class='text-xl font-bold text-red-600 mb-4'>Error</h2>
             <p class='mb-4 text-gray-700'>{mensaje}</p>
             <button
-              type="button"
+              type='button'
               class='absolute top-2 right-2 text-gray-500 hover:text-black'
               onClick={() => mostrar.value = false}
-            >✕</button>
+            >
+              ✕
+            </button>
           </div>
         </div>
       )}
@@ -26,9 +28,8 @@ export function ModalError({ mensaje }: Props) {
   )
 }
 
-
 interface LinkProps extends Props {
-  enlace: string,
+  enlace: string
   textoEnlace: string
 }
 
@@ -42,12 +43,16 @@ export function ModalLink({ mensaje, enlace, textoEnlace }: LinkProps) {
           <div class='bg-green-100 p-6 rounded-lg shadow-xl max-w-sm w-full relative select-none'>
             <h2 class='text-xl font-bold text-green-800 mb-4'>Aviso</h2>
             <p class='mb-4 text-gray-700'>{mensaje}</p>
-            <button type="button" class="bg-green-300 hover:bg-green-400 transition-colors duration-150 p-2 rounded-md"><a href={enlace}>{textoEnlace}</a></button>
+            <button type='button' class='bg-green-300 hover:bg-green-400 transition-colors duration-150 p-2 rounded-md'>
+              <a href={enlace}>{textoEnlace}</a>
+            </button>
             <button
-              type="button"
+              type='button'
               class='absolute top-2 right-2 text-gray-500 hover:text-black'
               onClick={() => mostrar.value = false}
-            >✕</button>
+            >
+              ✕
+            </button>
           </div>
         </div>
       )}
